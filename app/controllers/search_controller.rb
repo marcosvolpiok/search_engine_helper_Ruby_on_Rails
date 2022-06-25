8166d74e5d10bf9d912fb7b25b443ae47079c252
Marcos Volpi <marcos@marcosvolpi.com>
2022-06-25 15:41:18 -0300

    Add scaffolding

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,17 @@
-class ArticlesController < ApplicationController
+class SearchController < ApplicationController
     def search
-  
+      puts "q:::::::::::::"+params[:q]
+      puts params[:engine]
+      
+
+      engine = params[:engine].upcase
+      if engine == "GOOGLE"
+        render :inline => "searching in google"
+      elsif engine == "BING"
+        render :inline => "bing"
+      elsif engine == "GOOGLEBING" || engine == "BINGGOOGLE"
+        render :inline => "both"
+      end
     end
  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,20 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get 'blog' => 'articles#index'
-  get 'blog/:id' => 'articles#show'
+  get 'search/:engine/:q' => 'search#search'
 
-  post 'blog/comment/:id' => 'comments#create'
-
-
-  scope 'admin' do
-    root to: 'articles#index_admin' 
-
-    resources :articles, only: %i[create new edit update] do
-      collection do
-        get ':id/delete' => 'articles#delete'
-        patch '/' => 'articles#update'
-      end
-    end
-  end
 end
