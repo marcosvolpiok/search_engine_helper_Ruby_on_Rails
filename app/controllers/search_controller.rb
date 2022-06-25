185b8337763533abcb6e4a481faa578ccb20b1e5
Marcos Volpi <marcos@marcosvolpi.com>
2022-06-25 16:13:09 -0300

    Add HTTP fetching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,23 @@
 class SearchController < ApplicationController
-    def search
-      puts "q:::::::::::::"+params[:q]
-      puts params[:engine]
-      
+  require 'net/http'
+  require 'uri'
 
-      engine = params[:engine].upcase
-      if engine == "GOOGLE"
-        render :inline => "searching in google"
-      elsif engine == "BING"
-        render :inline => "bing"
-      elsif engine == "GOOGLEBING" || engine == "BINGGOOGLE"
-        render :inline => "both"
-      end
+  def search
+    engine = params[:engine].upcase
+    #content = Net::HTTP.get(URI.parse(''))
+    #puts content
+    config = {
+      GOOGLE: {url: 'https://www.google.com/search?q='},
+      BING: {url: 'https://www.bing.com/search?q='}
+    }
+
+
+    if engine == "GOOGLE" || engine == "BING"
+      content = Net::HTTP.get(URI.parse("#{config[engine.to_sym][:url]}#{params[:q]}"))
+      render :inline => content.force_encoding('iso-8859-1')
+    elsif engine == "GOOGLEBING" || engine == "BINGGOOGLE"
+      render :inline => "both"
     end
+  end
  
 end
